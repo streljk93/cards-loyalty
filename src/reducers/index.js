eddb5b1a714a135bd08ef6920e2b7932fc909227
streljk93 <strel.jk.93@gmail.com>
2018-09-25 18:28:32 +0300

    fixed reducer index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,58 +7,6 @@ import rule from './rule';
 import ruleType from './ruleType';
 import action from './action';
 
-// const tree = {
-//     ui: {
-//         messages: [{
-//             code: 404,
-//
-//         }]
-//     },
-//     cardType: {
-//         selected: 1,
-//         data: [{
-//             id: 1,
-//             slug: 'bonus',
-//             image: 'https://image.ru/img.jpeg',
-//             name: 'Бонусная карта',
-//             description: 'Desc...',
-//             qrcode: 'qr',
-//         }],
-//         meta: {
-//             isFetching: false,
-//             receivedAt: 2342432,
-//         },
-//     },
-//     cardStore: {
-//         data: [{
-//             id: 1,
-//             card_type_id: 1,
-//             store_id: 1,
-//             image: '',
-//         }],
-//         meta: {
-//             isFetching: false,
-//             receivedAt: 23424234,
-//         },
-//     },
-//     cardUser: [{
-//         id: 1,
-//         card_store_id: 1,
-//         user_id: 1,
-//         number: 1,
-//     }],
-//     user: [{
-//         store_id: 1,
-//         info: 'adfasf',
-//     }],
-//     store: {
-//         id: 1,
-//         slug: 'jk',
-//         name: 'JK Magazine',
-//         token: 'JWT ==akdjfkadfsfjadjafja.sadfaskdfj.sasadfasdf',
-//     },
-// };
-
 export default combineReducers({
     routing: routerReducer,
     ui,
